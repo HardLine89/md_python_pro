fdf88399639fbde9ba8539c5e6f29142fb7d9242
HardLine89 <dmitrymakarov089@gmail.com>
2025-02-26 18:18:02 +0600

    Сигнал создания юзера качает аватар, работа с шаблонами

--- utils/signals.py
+++ utils/signals.py
@@ -1,3 +1,5 @@
+import requests
+from django.core.files.base import ContentFile
 from django.db.models.signals import post_save
 import random
 import unidecode
@@ -20,6 +22,7 @@ def populate_profile_on_signup(request, user, sociallogin=None, **kwargs):
         first_name = extra_data.get("given_name", "").strip()
         last_name = extra_data.get("family_name", "").strip()
         email = extra_data.get("email", "").strip()
+        avatar_url = extra_data.get("picture", "")
 
         # Если нет имени и фамилии, берем часть email до "@"
         if not first_name and not last_name and email:
@@ -34,7 +37,10 @@ def populate_profile_on_signup(request, user, sociallogin=None, **kwargs):
             # Заполняем имя и фамилию
             user.first_name = first_name
             user.last_name = last_name
-
+        if avatar_url:
+            avatar_file = download_avatar(avatar_url)
+            if avatar_file:
+                user.profile.avatar.save(avatar_file.name, avatar_file, save=True)
         # Привязываем email
         if email and not user.email:
             user.email = email
@@ -48,6 +54,18 @@ def generate_unique_username(base_username):
     while User.objects.filter(username=username).exists():
         username = f"{base_username}{random.randint(1000, 9999)}"
     return username
+
+
+def download_avatar(url):
+    """
+    Загружает аватар по URL и возвращает объект ContentFile для сохранения в ImageField.
+    """
+    try:
+        response = requests.get(url, timeout=5)
+        response.raise_for_status()  # Проверяем статус ответа
+        return ContentFile(response.content, name=url.split("/")[-1])
+    except requests.RequestException:
+        return None  # В случае ошибки просто не сохраняем аватар
 
 
 @receiver(post_save, sender=User)
