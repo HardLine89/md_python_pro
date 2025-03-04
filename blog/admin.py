5bb2f6c1f5591d505d7409d866d6c3fcab943a25
HardLine89 <dmitrymakarov089@gmail.com>
2025-03-04 09:45:06 +0600

    Мелкие правки профиля и админки

--- blog/admin.py
+++ blog/admin.py
@@ -1,19 +1,8 @@
 from django.contrib import admin
-from django.contrib.contenttypes.admin import GenericTabularInline
 from django.contrib.contenttypes.models import ContentType
 from django.utils.html import format_html
-
 from blog.models import Article, Category
-from comments.models import Comment
 from ratings.models import LikeDislike
-
-
-class CommentInline(GenericTabularInline):
-    model = Comment
-    extra = 0
-    readonly_fields = ("created_at", "updated_at")
-    fields = ("author", "parent", "text", "created_at", "updated_at")
-    can_delete = True
 
 
 @admin.register(Article)
@@ -26,7 +15,6 @@ class ArticleAdmin(admin.ModelAdmin):
         "updated_at",
         "views",
         "votes_info",
-        # "comments_info",
     )
     list_filter = ("category", "author")
     search_fields = ("title", "content")
@@ -38,41 +26,33 @@ class ArticleAdmin(admin.ModelAdmin):
         "updated_at",
         "views",
         "votes_info",
-        # "comments_info",
     )
-    # inlines = [CommentInline]
     fieldsets = [
-        (None, {"fields": ["title", "slug", "category", "content", "tags", "author", "cover"]}),
+        (
+            None,
+            {
+                "fields": [
+                    "title",
+                    "slug",
+                    "category",
+                    "content",
+                    "tags",
+                    "author",
+                    "cover",
+                ]
+            },
+        ),
         (
             "Статистика",
             {
                 "fields": [
                     "votes_info",
                     "views",
-                    # "comments_info",
                 ]
             },
         ),
         ("Даты", {"fields": ["created_at", "updated_at"]}),
     ]
-
-    # def comments_info(self, obj):
-    #     """
-    #     Возвращает количество комментариев для статьи.
-    #     """
-    #     comments_count = obj.comments.count()
-    #
-    #     # Получаем content_type для модели obj
-    #     content_type = ContentType.objects.get_for_model(obj)
-    #
-    #     return format_html(
-    #         '<a href="/admin/comments/comment/?content_type__id__exact={}&object_id={}">Комментарии: {}</a>',
-    #         content_type.id,  # Используем ID вместо model name
-    #         obj.id,
-    #         comments_count,
-    #     )
-
-    # comments_info.short_description = "Комментарии"
 
     def votes_info(self, obj):
         """
